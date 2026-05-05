eb453af7ab8e2e225758c893ca72a132c18fb280
Yanishenry-png <Yanishenry@outlook.fr>
2026-05-05 04:11:30 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+import os
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Connecté en tant que {bot.user}")
+
+@bot.command()
+async def salut(ctx):
+    await ctx.send("salut")
+
+bot.run(os.environ["TOKEN"])
